5b54228544dd35327c5b80f3911addf233e4f951
Ahnaf-Karim <karima3@rpi.edu>
2025-12-06 11:38:26 -0500

    lab 9

---
+++ Lab09/iit-lab9start.sql
@@ -0,0 +1,30 @@
+-- create the tables for our movies
+CREATE TABLE `movies` (
+   `movieid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+   `title` varchar(100) NOT NULL,
+   `year` char(4) DEFAULT NULL,
+   PRIMARY KEY (`movieid`)
+);
+-- insert data into the tables
+INSERT INTO movies
+VALUES (1, "Elizabeth", "1998"),
+   (2, "Black Widow", "2021"),
+   (3, "Oh Brother Where Art Thou?", "2000"),
+   (
+      4,
+      "The Lord of the Rings: The Fellowship of the Ring",
+      "2001"
+   ),
+   (5, "Up in the Air", "2009");
+
+-- create the tables for our actors
+CREATE TABLE `actors` (
+   `actorid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+   `firstNames` varchar(100) NOT NULL,
+   `lastName` varchar(100) DEFAULT NULL,
+   `dob` date DEFAULT NULL
+   PRIMARY KEY (`actorid`)
+);
+-- insert data into the tables
+INSERT INTO actors
+VALUES (1, "Keanu" "Reaves", "1998-10-13");
